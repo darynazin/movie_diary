76fb7fb15db381401f8db04d7244cdbea549f901
darynazin <danayu@ukr.net>
2024-12-05 20:27:46 +0100

    adjusted some styles

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,12 +1,15 @@
-const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-
 export const addToStorage = (movie) => {
+  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
   favorites.push(movie);
   localStorage.setItem('favorites', JSON.stringify(favorites));
 };
 
 export const deleteFromStorage = (movie) => {
+  const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
   const index = favorites.findIndex(fav => fav.id === movie.id);
   favorites.splice(index, 1);
   localStorage.setItem('favorites', JSON.stringify(favorites));
 }
+
+
+
